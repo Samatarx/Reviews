a49ef85e8b0c940f088b7d95469606654a0a7a63
Samatar Xasan <Samatar.Xasan@gmail.com>
2021-02-02 11:36:15 +0000

    Updates to CSS and dummy text

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import Review from './Review';
+import "./App.css";
+import Review from "./Review";
 
 function App() {
   return (
     <main className="App">
       <section className="container">
         <div className="title">
-        <h2>Our reviews</h2>
-        <div className="underline"></div>
+          <h2>Our reviews</h2>
+          <div className="underline"></div>
         </div>
-        <Review/>
+        <Review />
       </section>
     </main>
   );
--- src/data.js
+++ src/data.js
@@ -1,40 +1,40 @@
 const reviews = [
-    {
-      id: 1,
-      name: 'susan smith',
-      job: 'web developer',
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-      text:
-        "Lorem perspiciatis. Inventore ea ducimus laborum ex, hic quaerat officiis aliquam repellendus? Consequuntur nesciunt porro temporibus itaque odit? Itaque dicta similique quisquam neque ad ea quis exercitationem reprehenderit cumque dolor.",
-    },
-    {
-      id: 2,
-      name: 'anna johnson',
-      job: 'web designer',
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
-      text:
-        'exercitationem pariatur sint alias repellat porro voluptatum consectetur mollitia suscipit molestias perspiciatis. Inventore ea ducimus laborum ex, hic quaerat officiis aliquam repellendus? Consequuntur nesciunt porro temporibus itaque odit? Itaque dicta.',
-    },
-    {
-      id: 3,
-      name: 'peter jones',
-      job: 'intern',
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-      text:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis unde molestiae eius nobis laboriosam, corrupti voluptate cupiditate veniam dolorum animi eveniet recusandae minus molestias. Repudiandae, nesciunt cum. Corrupti repudiandae, obcaecati sint enim vero magnam laudantium?",
-    },
-    {
-      id: 4,
-      name: 'bill anderson',
-      job: 'the boss',
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
-      text:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt rerum sit iste! Doloremque esse, praesentium non eligendi voluptatum amet voluptatem molestiae, ratione dolorum, facilis cupiditate dolores distinctio nesciunt expedita? Saepe ut animi, autem laboriosam facilis doloribus voluptate esse similique expedita maxime, fugit, deleniti nobis vero!",
-    },
-  ];
-  
-  export default reviews;
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text:
+      "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisit",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text:
+      "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary re",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text:
+      "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed ",
+  },
+];
+
+export default reviews;
